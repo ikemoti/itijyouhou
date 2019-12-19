1bfa65e1f45903a3dc72596c6a13176d994004b3
ikemoti <ikepon413@icloud.com>
2019-12-19 09:25:35 +0900

    Initial Commit

---
+++ itijyouhou/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  itijyouhou
+//
+//  Created by USER on 2019/12/19.
+//  Copyright © 2019 USER. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
