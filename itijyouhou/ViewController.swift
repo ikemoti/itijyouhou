80eba4c33c352da6ed791e37a4217fae2a475d64
ikemoti <ikepon413@icloud.com>
2020-01-13 13:44:39 +0900

    検索範囲追加

--- itijyouhou/ViewController.swift
+++ itijyouhou/ViewController.swift
@@ -9,12 +9,17 @@
 import UIKit
 import CoreLocation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UIPickerViewDelegate,UIPickerViewDataSource {
     
     //緯度、経度表示するラベル
     @IBOutlet weak var latitude: UILabel!
     
     @IBOutlet weak var lonitude: UILabel!
+    
+    @IBOutlet weak var pickerView: UIPickerView!
+    
+    @IBOutlet weak var pickerLabel: UILabel!
+    
     //変数設置
     var latitudeNow : String = ""
     var lonitudeNow : String = ""
@@ -27,7 +32,9 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         setupLocationManager()
-       
+        pickerView.delegate = self
+        pickerView.dataSource = self
+        pickerLabel.text = "デフォルト"
     }
     
     
@@ -47,14 +54,10 @@ class ViewController: UIViewController {
     
     func setupLocationManager(){
         locationManager = CLLocationManager()
-    //
         guard let locationManager = locationManager else {return}
-        locationManager.requestWhenInUseAuthorization()
-    //
+    locationManager.requestWhenInUseAuthorization()
         let status = CLLocationManager.authorizationStatus()
-    //
         if status == .authorizedWhenInUse{locationManager.delegate = self as? CLLocationManagerDelegate }
-    //
         locationManager.startUpdatingLocation()
         
 
@@ -64,21 +67,36 @@ class ViewController: UIViewController {
 func showAlert(){
     let alertTitle = "位置情報が許可されていません"
     let alertMessage = "設定アプリの「プライバシー＞位置情報サービス」から変更してください"
-    
     let alert:UIAlertController = UIAlertController(
         title:  alertTitle,
         message: alertMessage,
         preferredStyle: UIAlertController.Style.alert)
-    
     let defaultAction:UIAlertAction = UIAlertAction (
         title:"OK",
         style: UIAlertAction.Style.default,
         handler: nil )
-    
     alert.addAction(defaultAction)
-    
     present (alert , animated: true , completion: nil)
 }
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return dataList.count
+    }
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return dataList[row]
+    }
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        pickerLabel.text = dataList[row]
+    }
+    
+    
+    
+    
+    
+    
 }
 extension ViewController : CLLocationManagerDelegate{
     
